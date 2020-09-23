60135f46557ccceb81613131eddd402430148dda
Caio Alexandre <contato.kaiyo@gmail.com>
2020-09-23 16:51:47 -0300

    Create file-based logging system

--- pearl/pearl.py
+++ pearl/pearl.py
@@ -21,6 +21,32 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 '''
+'''
+The MIT License (MIT)
+
+Copyright (c) 2015 Rapptz
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+'''
+
+import contextlib
+import logging
 
 import discord
 from discord.ext import commands
@@ -38,10 +64,38 @@ class Pearl(commands.Bot):
         super().__init__(command_prefix=get_prefix)
 
 
+@contextlib.contextmanager
+def setup_logging() -> None:
+    """Setup a file-based logging."""
+    try:
+        # __enter__
+        dt_format = '%Y-%m-%d %H:%M:%S'
+
+        logging.getLogger('discord').setLevel(logging.INFO)
+        logging.getLogger('discord.http').setLevel(logging.WARN)
+    
+        logger = logging.getLogger()
+        logger.setLevel(logging.INFO)
+
+        handler = logging.FileHandler(filename='pearl.log', encoding='utf-8', mode='w')
+        formatter = logging.Formatter('[{asctime}] [{levelname}] {name}: {message}', dt_format, style='{')
+    
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+        yield
+    finally:
+        # __exit__
+        for handler in logger.handlers:
+            handler.close()
+            logger.removeHandler(handler)
+
+
 def main():
     pearl = Pearl()
     pearl.run(config.token)
 
 
 if __name__ == '__main__':
-    main()
+    with setup_logging():
+        main()
